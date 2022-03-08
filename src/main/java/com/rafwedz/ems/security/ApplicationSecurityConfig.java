c7e33a00ba2f2d82528e085757c7791f11ba6166
julesYVES237 <juyv02@gmail.com>
2022-03-08 14:24:53 +0100

    modif origin91

--- src/main/java/com/rafwedz/ems/security/ApplicationSecurityConfig.java
+++ src/main/java/com/rafwedz/ems/security/ApplicationSecurityConfig.java
@@ -2,7 +2,13 @@
  * package that contains config utils
  */
 package com.rafwedz.ems.security;
-
+import javax.servlet.Filter ;
+        import javax.servlet.FilterChain ;
+import javax.servlet.ServletException ;
+import javax.servlet.ServletRequest ;
+import javax.servlet.ServletResponse ;
+import javax.servlet.http.HttpServletResponse ;
+import java.io.IOException ;
 import com.rafwedz.ems.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +17,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 
 @Configuration
@@ -52,12 +58,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/employees","/employees/{id}","/auth/login","/tasks","/tasks/count","/tasks/tasksdto",
                         "/tasks/unassigned","/tasks/unassigned/count","/tasks/assigned/count","/tasks/done",
                         "/tasks/new","/tasks/assigned","/task/assigned","/tasks/{task_id}","/tasks/nn/{employee_id}","/employees/{emp_id}/tasks","/employees/count","/employees/wages","/employees/{emp_id}/tasks/all","/employees/{emp_id}/tasks/done","/employees/{emp_id}/tasks/new").permitAll()
-                .anyRequest().authenticated()
-                .and()
-         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-               .and()
-               .authorizeRequests().and().httpBasic();
-       http.headers().cacheControl();
+                .anyRequest().authenticated();
 
 
     }
@@ -67,6 +68,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             final AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService);
 
+    }
+    @Component
+    public class CorsFilter implements Filter {
+
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+            chain.doFilter(req, res);
+        }
     }
 
     @Bean
