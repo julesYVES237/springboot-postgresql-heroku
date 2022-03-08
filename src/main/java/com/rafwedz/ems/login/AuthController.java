595341e32c76e038f458e117c65df68df09c6297
julesYVES237 <juyv02@gmail.com>
2022-03-08 12:43:33 +0100

    modif origin7

--- src/main/java/com/rafwedz/ems/login/AuthController.java
+++ src/main/java/com/rafwedz/ems/login/AuthController.java
@@ -7,23 +7,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://spring-ems1.herokuapp.com/", allowedHeaders = "*")
+@CrossOrigin
 @AllArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-    @CrossOrigin(origins = "https://spring-ems1.herokuapp.com/", allowedHeaders = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws Exception  {
         return authService.login(loginRequest);
     }
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/auth")
-                .allowedOrigins("https://spring-ems1.herokuapp.com/")
-                .allowedMethods("PUT", "DELETE","POST")
-                .allowedHeaders("header1", "header2", "header3")
-                .exposedHeaders("header1", "header2")
-                .allowCredentials(false).maxAge(3600);
-    }
 }
